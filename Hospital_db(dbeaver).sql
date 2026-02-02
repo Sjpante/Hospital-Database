149ce0fa9fae2c159d2904a98ef292fad0adab3a
Sjpante <sjpante@gmail.com>
2026-02-02 14:29:17 +0200

    Delete Hospital_db(dbeaver).sql

--- Hospital_db(dbeaver).sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  p.PatientID,
-  CONCAT(p.FirstName, ' ', p.LastName) AS PatientName,
-  COUNT(pr.PrescriptionID) AS PrescriptionCount
-FROM PATIENT p
-LEFT JOIN PRESCRIPTION pr ON pr.PatientID = p.PatientID
-GROUP BY p.PatientID, p.FirstName, p.LastName
-ORDER BY PrescriptionCount DESC;
